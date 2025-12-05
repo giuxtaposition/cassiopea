4daabca391fdbbd0cb03a0d094211b2aea261961
giuxtaposition <yg97.cs@gmail.com>
2025-12-05 22:20:44 +0100

    feat(bar): hide/show tray on click

--- src/widgets/Tray.tsx
+++ src/widgets/Tray.tsx
@@ -1,6 +1,6 @@
 import Gtk from "gi://Gtk?version=4.0"
 import AstalTray from "gi://AstalTray"
-import { For, createBinding } from "ags"
+import { For, createBinding, createState } from "ags"
 
 const IconNameMapping: Record<string, string> = {
   "blueman-tray": "bluetooth-symbolic",
@@ -8,7 +8,13 @@ const IconNameMapping: Record<string, string> = {
 
 export function Tray() {
   const tray = AstalTray.get_default()
+  const [visible, setVisible] = createState(false)
   const items = createBinding(tray, "items")
+
+  const icon = (visible: boolean) => {
+    if (visible) return "chevron-right-symbolic"
+    else return "chevron-left-symbolic"
+  }
 
   const init = (btn: Gtk.MenuButton, item: AstalTray.TrayItem) => {
     btn.menuModel = item.menuModel
@@ -19,26 +25,37 @@ export function Tray() {
   }
 
   return (
-    <box>
-      <For each={items}>
-        {(item) => {
-          const iconName = createBinding(item, "iconName")
-          const mappedIconName = IconNameMapping[iconName.peek()]
-          return (
-            <menubutton $={(self) => init(self, item)}>
-              {mappedIconName ? (
-                <image
-                  iconName={iconName((icon) => IconNameMapping[icon])}
-                  gicon={item.get_gicon()}
-                  pixelSize={16}
-                />
-              ) : (
-                <image gicon={item.get_gicon()} pixelSize={16} />
-              )}
-            </menubutton>
-          )
-        }}
-      </For>
+    <box spacing={2}>
+      <button onClicked={() => setVisible((v) => !v)}>
+        <image iconName={visible((v) => icon(v))} pixelSize={20} />
+      </button>
+      <revealer
+        revealChild={visible}
+        transitionType={Gtk.RevealerTransitionType.SLIDE_RIGHT}
+        transitionDuration={200}
+      >
+        <box spacing={2}>
+          <For each={items}>
+            {(item) => {
+              const iconName = createBinding(item, "iconName")
+              const mappedIconName = IconNameMapping[iconName.peek()]
+              return (
+                <menubutton $={(self) => init(self, item)}>
+                  {mappedIconName ? (
+                    <image
+                      iconName={iconName((icon) => IconNameMapping[icon])}
+                      gicon={item.get_gicon()}
+                      pixelSize={16}
+                    />
+                  ) : (
+                    <image gicon={item.get_gicon()} pixelSize={16} />
+                  )}
+                </menubutton>
+              )
+            }}
+          </For>
+        </box>
+      </revealer>
     </box>
   )
 }
