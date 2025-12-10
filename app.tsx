282a3b6c43c902f140bb4dabfecbb70203f868a0
giuxtaposition <yg97.cs@gmail.com>
2025-12-10 19:00:09 +0100

    feat(cli): handle requests

--- app.tsx
+++ app.tsx
@@ -4,10 +4,20 @@ import Bar from "./src/widgets/Bar"
 import { createBinding, For, This } from "ags"
 import Applauncher from "./src/widgets/Applauncher"
 import NotificationPopups from "./src/widgets/NotificationPopups"
+import { toggleWindow, WindowName } from "./src/utils/window"
 
 app.start({
+  instanceName: "cassiopea",
   css: style,
   icons: `${SRC}/icons`,
+  requestHandler(argv: string[], response: (response: string) => void) {
+    const [cmd, arg, ...rest] = argv
+    if (cmd == "toggle") {
+      toggleWindow(arg as WindowName)
+      return response(arg)
+    }
+    response("unknown command")
+  },
   main() {
     const monitors = createBinding(app, "monitors")
 
